f4e9c7dc9dfb7da32fa6f239afaf8e24fbeaee6f
Sandeep Tiwari <sandeepatiwari12@gmail.com>
2022-04-19 12:09:56 +0530

    message updated

--- server/index.js
+++ server/index.js
@@ -8,5 +8,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 app.listen(port, () => {
-  console.log("Server is up!");
+  console.log("Server started successfully");
 });
